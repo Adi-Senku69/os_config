0373a9e39355ff8bc5e8e93d9d21c1e1afb93115
Adi-Senku69 <vaditya06@gmail.com>
2025-06-18 18:35:43 +0530

    Added config to install virtualbox and vagrant

---
+++ addition_software.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install Virtualbox
+sudo apt install virtualbox -y
+
+# Install vagrant
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant -y
